f7d17d962bd5d041c244e48afa524e44c71bc4aa
Евгений <80124915+TheCotOfTehas@users.noreply.github.com>
2023-05-07 22:05:14 +0300

    Написал решение первого задания

--- application/CSharpApp/Program.cs
+++ application/CSharpApp/Program.cs
@@ -2,18 +2,68 @@
 //VALUES (                54173,N'Zurine',N'White', N'Female',N'Spanish', 693,           CAST(N'2016-07-08 10:51:03' AS DateTime),N'myData.biz')
 //Console.WriteLine(s);
 //Console.WriteLine(new string('-', 100));
+//var r = Startup.GetListCommandInsert("C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
+//Startup.Start("PersonDB", "C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
+//Starter.Start("PersonDB");
+//Startup.ClearTable("PersonDB", "C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
+
 
 using System.Data.SqlClient;
 using System.Configuration;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using CSharpApp;
+using static Azure.Core.HttpHeader;
 
-var r = Startup.GetListCommandInsert("C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
-//Startup.Start("PersonDB", "C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
-//Starter.Start("PersonDB");
-//Startup.ClearTable("PersonDB", "C:\\Users\\tsebr\\OneDrive\\Рабочий стол\\foreign_names.sql");
-foreach (var item in r)
+string connectionConfiguration = ConfigurationManager.ConnectionStrings["PersonDB"].ConnectionString;
+SqlConnection sqlConnection = new SqlConnection(connectionConfiguration);
+
+Console.WriteLine("Добро пожаловать, Вы  запустили консольное приложение для выполнения тестового задания");
+//var myApp2 = new Starter2();
+//var myApp3 = new Starter3();
+//var myApp4 = new Starter4();
+//var myApp5 = new Starter5();
+//var myApp6 = new Starter6();
+
+while (true)
 {
-    Console.WriteLine(item);
+    Console.WriteLine("Введите комманду запуска приложенния");
+    Console.Write(", ");
+
+    var command = Console.ReadLine();
+
+    if (command.ToLower().Equals("exit"))
+        break;
+
+    switch (command.ToLower())
+    {
+        case "csharp myapp.cs 1":
+            var myApp1 = new Starter1(sqlConnection);
+            myApp1.Run();
+            break;
+
+        //case "CSharp myApp.cs 2":
+        //    myApp2.Run();
+        //    break;
+
+        //case "CSharp myApp.cs 3":
+        //    myApp3.Run();
+        //    break;
+
+        //case "CSharp myApp.cs 4":
+        //    myApp4.Run();
+        //    break;
+
+        //case "CSharp myApp.cs 5":
+        //    myApp5.Run();
+        //    break;
+
+        //case "CSharp myApp.cs 6":
+        //    myApp6.Run();
+        //    break;
+
+        default:
+            Console.WriteLine($"Комманада {command} была не корректной");
+            break;
+    }
 }
---
+++ application/CSharpApp/Starter1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CSharpApp
+{
+    public class Starter1
+    {
+        SqlConnection sqlConnection;
+        public Starter1(SqlConnection sqlConnection)
+        {
+            this.sqlConnection = sqlConnection;
+        }
+        public void Run()
+        {
+            Console.WriteLine("Введите название таблици");
+            Console.Write(", ");
+            string nameTable = Console.ReadLine();
+
+            string command = $"CREATE TABLE [dbo].[{nameTable}] (\r\n" +
+                            "[Id] INT IDENTITY (1, 1) NOT NULL,\r\n" +
+                            "[Name] NCHAR (20) NULL,\r\n" +
+                            "[Surname] NCHAR (20) NULL,\r\n" +
+                            "[Patronymic] NCHAR (20) NULL,\r\n" +
+                            "[Gender] NCHAR (20) NULL,\r\n" +
+                            "[DateBirth]  DATE       NULL,\r\n" +
+                            "PRIMARY KEY CLUSTERED ([Id] ASC)\r\n);";
+
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            Console.WriteLine($"Создана  {sqlCommand.ExecuteNonQuery()} таблица");
+            sqlConnection.Close();
+        }
+    }
+}
